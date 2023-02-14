44b1f01defcdd9d7646bbca063dd648e347f6853
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-14 15:56:26 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "1fab7927-231e-4e14-a991-2bdbfac5dca6",
+    "home_feed": [
+        {
+            "id": "da876a1d-6592-4585-b2aa-bcf5e6ede41a",
+            "artist_name": "common",
+            "home_carousel": [
+                {
+                    "id": "d2dd1de5-7e23-4ff0-9796-08be8aee5189",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/33.png"
+                },
+                {
+                    "id": "037c0c7f-385d-4f48-b734-be8be93e3f00",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png"
+                },
+                {
+                    "id": "d741aac2-6bc1-49bd-8d6b-2feea5f8b4bf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/136.png"
+                },
+                {
+                    "id": "72ac060d-3fab-485f-88a9-e9dfcfceaf67",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "e0dae476-0da6-44a2-badc-e82f15fef1d6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png"
+                }
+            ],
+            "art_caption": "religious",
+            "art_description": "Compare star nice left. Hit me design effort."
+        },
+        {
+            "id": "70544717-149d-40f1-b6c3-3ec9e64daea6",
+            "artist_name": "cultural",
+            "home_carousel": [
+                {
+                    "id": "8c5d7def-db4f-4d52-b2f9-9fdd1e9ebdb8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/78.png"
+                },
+                {
+                    "id": "d94d6fd8-f1af-4d5e-abf3-aa703dc524de",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "49de1848-0b5b-4867-9d97-0d5455405dad",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                },
+                {
+                    "id": "323124e8-d799-4697-8b39-a857d089d03e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/6.png"
+                },
+                {
+                    "id": "e762fa8d-92f9-4d4f-b0aa-2de1999b5632",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                }
+            ],
+            "art_caption": "fight",
+            "art_description": "Her trial yes military. Itself own step assume national. Dream ask executive maintain forward sing produce last. Model production develop left number."
+        },
+        {
+            "id": "229ea91d-dcbb-4e7a-826a-8570bf5f295a",
+            "artist_name": "light",
+            "home_carousel": [
+                {
+                    "id": "bbf7fdcd-fc22-4ee4-82c9-b9d3ef1954ed",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/79.png"
+                },
+                {
+                    "id": "42748601-200f-4f75-9ff9-52a36c448b88",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/257.png"
+                },
+                {
+                    "id": "269e86b5-5031-4994-b319-c0030d74d1ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/152.png"
+                },
+                {
+                    "id": "b566c2ac-302b-4dcf-ade7-8a408c5853d5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/240.png"
+                },
+                {
+                    "id": "4a278743-103f-42eb-bc6b-7ac1d7093379",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png"
+                }
+            ],
+            "art_caption": "herself",
+            "art_description": "Worry among couple kid dinner around tend special. Participant statement win must call. Employee miss various despite your."
+        },
+        {
+            "id": "3d43aa99-d4c9-4f64-9bad-1e034e5164f1",
+            "artist_name": "we",
+            "home_carousel": [
+                {
+                    "id": "d4306f96-41c4-4586-805f-38a7e7e3a8e8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/119.png"
+                },
+                {
+                    "id": "f7839686-0641-4f20-991c-36d0f597b228",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "1d57a75d-c403-4e2b-851c-243a18094a71",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png"
+                },
+                {
+                    "id": "b217a4c9-5d55-4104-942f-4e551ee16d8c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "7b2490e4-832a-4915-8b14-3b7d32bff509",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                }
+            ],
+            "art_caption": "test",
+            "art_description": "Former get red with fill. Course many sign best west region."
+        },
+        {
+            "id": "dadcab94-d499-4236-bae5-82c2440a0b20",
+            "artist_name": "various",
+            "home_carousel": [
+                {
+                    "id": "d7979afd-5f0f-450a-be96-4e069c45eaee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/335.png"
+                },
+                {
+                    "id": "4d5ffcc9-bf73-4f05-978b-cbaf7f058a51",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/254.png"
+                },
+                {
+                    "id": "646ea34b-b3b1-48e8-a369-adbfca607f0b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "61840f39-3ed1-4726-96ee-95cb3e2829ec",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/216.png"
+                },
+                {
+                    "id": "b4687721-4eed-4e8e-baf2-17ba5645f0d0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/201.png"
+                }
+            ],
+            "art_caption": "weight",
+            "art_description": "Century determine I value. Memory find name cover way."
+        }
+    ]
+}
